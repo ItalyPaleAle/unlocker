38b80e490ca27bdc2174ed9004dc6c344efd40c8
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2022-06-27 22:33:00 -0700

    Minify CSS in prod

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,10 @@
+const production = process.env.NODE_ENV == 'production'
+
 module.exports = {
     plugins: {
         'postcss-import': {},
         'postcss-url': {},
         tailwindcss: {},
+        ...(production ? {cssnano: {}} : {})
     },
 }
