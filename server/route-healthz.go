06a585ceb99a6b75f02aac392ee22f1902b849cb
Alessandro Segala (ItalyPaleAle) <43508+ItalyPaleAle@users.noreply.github.com>
2022-07-03 18:42:33 -0700

    Healthz endpoint includes app version

--- server/route-healthz.go
+++ server/route-healthz.go
@@ -1,19 +1,31 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/italypaleale/unlocker/buildinfo"
 )
 
 // RouteHealthz is the handler for the GET /healthz request
 // It can be used to ping the server and ensure everything is working
 func (s *Server) RouteHealthz(c *gin.Context) {
+	prod, _ := strconv.ParseBool(buildinfo.Production)
+
 	c.JSON(http.StatusOK, routeHealthzResponse{
-		Status: "ok",
+		Status:     "ok",
+		AppVersion: buildinfo.AppVersion,
+		Build:      fmt.Sprintf("%s, %s (%s)", buildinfo.BuildId, buildinfo.BuildDate, buildinfo.CommitHash),
+		Production: prod,
 	})
 }
 
 type routeHealthzResponse struct {
-	Status string
+	Status     string `json:"status"`
+	AppVersion string `json:"appVersion"`
+	Build      string `json:"build"`
+	Production bool   `json:"production"`
 }
