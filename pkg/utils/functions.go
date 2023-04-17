3f94efba43713174d9f0895aa2b7ea887dc8eae0
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-04-16 20:43:19 -0700

    Small tweaks

--- pkg/utils/functions.go
+++ pkg/utils/functions.go
@@ -7,6 +7,10 @@ import (
 
 // IsTruthy returns true if a string is truthy, such as "1", "on", "yes", "true", "t", "y"
 func IsTruthy(str string) bool {
+	if len(str) > 4 {
+		// Short-circuit to avoid processing strings that can't be true
+		return false
+	}
 	switch strings.ToLower(str) {
 	case "1", "true", "t", "on", "yes", "y":
 		return true
